76facb89fb2ffca62ccf61c8d03a0b32cdbc1439
Manoj.TV <Manoj.TV@deltaww.com>
2024-07-23 18:09:02 +0530

    new meyhod

--- learnexpress/index.js
+++ learnexpress/index.js
@@ -1,11 +1,22 @@
 import express from "express"
+import mongoose from "mongoose";
+import { MongoClient, ServerApiVersion} from "mongodb";
 const app = express();
+const uri = "mongodb+srv://manojtvmtv:DuyrGfMFYmMZ4tGF@cluster0.ezugu8a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 app.listen(3000, () => {
  console.log("App is running");
 });
 
-app.get('/', (req,res) => {
-    console.log(req.rawHeaders);
-res.send("hello form get");
-})
+app.get('/', async (req, res) => {
+    try {
+      await mongoose.connect(uri, {
+        serverApi: ServerApiVersion.v1
+      });
+      console.log('Mongoose connected to MongoDB');
+      res.send("Hello from GET with success");
+    } catch (error) {
+      console.error('Mongoose connection error:', error);
+      res.send("Hello from GET with error");
+    }
+  });
